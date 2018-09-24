c1016d5e6bf26eef8034a6d6e70c0b4e066f25e0
Tibau vdb <tibau.van.den.broeck@student.howest.be>
2018-09-24 17:49:07 +0200

    Start adding movement

--- Assets/Scripts/CharacterControllerBehaviour.cs
+++ Assets/Scripts/CharacterControllerBehaviour.cs
@@ -6,16 +6,24 @@ using UnityEngine.Assertions;
 //[RequireComponent(typeof(CharacterController))] //Zorgt ervoor dat het script alleen maar werkt als er een characterController is
 public class CharacterControllerBehaviour : MonoBehaviour {
 
+    [SerializeField] //Serialized fields kunnen in de editor gezien worden
+    private Transform _absoluteTransform; //of relativeTransform
+
     private CharacterController _charCtrl;
     private Vector3 _velocity= Physics.gravity;
+    private Vector3 _inputMovement;
+
+
 	// Use this for initialization
 	void Start () {
+
         _charCtrl = GetComponent<CharacterController>();                                    //Generic Method (Als de component niet bestaat zal het veel geheugen verbruiken)
        // _charCtrl = (CharacterController)GetComponent(typeof(CharacterController));       //Non-Generic method (will crash if it doesn't work)
        // _charCtrl = GetComponent("CharacterController") as CharacterController;           //Non-Generic method (Will return null if it doesnt work)
 
         //typeof can tijdens compile time het klassentype teruggeven
         //(CharacterController) en typeof(CharacterController) zijn hetzelfde dus de functie kan returnen
+
 #if DEBUG
         //Conditional, zorgt ervoor dat al dan niet sommige stukken code worden meegenomen of niet
         /*if (_charCtrl == null)
@@ -25,25 +33,61 @@ public class CharacterControllerBehaviour : MonoBehaviour {
         }*/
         Assert.IsNotNull(_charCtrl, "DEPENDENCY ERROR: CharacterControllerBehaviour needs a CharacterControllerComponent"); //If object is null, assertion fails and error gets sent out
 #endif
+
 	}
 
+    private void Update()
+    {
+        //Input apart van wiskundige berekening houden | Inputs in update() doen omdat het een keer per frame wordt gedaan
+        _inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //Input vertalen tov een ander gameobject (bv Camera) | extra veld aanmaken om movement te kunnen vertalen
+    }
 
 
-
-
-
+    //Gravity changes should be done in FixedUpdate so it isn't dependent on FPS
+    //Abstractie-niveaus : kijken naar het huis of de bakstenen van het huis | huis beschrijven met ramen en deuren of beschrijven hoe de ramen en deuren gemaakt zijn en bij elkaar zitten
 
     // Update is called once per frame
-    void Update () {
-        if(!_charCtrl.isGrounded)
-        {
-        _velocity += Physics.gravity * Time.deltaTime; //Increment so that the velocity keeps increasing
-        }
-        Vector3 movement = _velocity * Time.deltaTime;
+    void FixedUpdate () {
 
+        Grounded();
 
-        _charCtrl.Move(movement);
+        Falling();
+
+        ApplyMovement();
+
+        DoMovement();
 	}
 
+    private void Grounded()
+    {
+        if (_charCtrl.isGrounded)
+        {
+            _velocity -= Vector3.Project(_velocity, Physics.gravity); //Project() projects a vector onto another vector | With dotproduct
+        }
+    }
 
+    private void Falling()
+    {
+        if (!_charCtrl.isGrounded)
+        {
+            _velocity += Physics.gravity * Time.fixedDeltaTime; //Increment so that the velocity keeps increasing
+                                                                //deltaTime is werkelijke frametime en fixeddeltatime is physics frametime (in fixed update zijn beide fixedDeltaTime)
+        }
+    }
+
+    private void ApplyMovement()
+    {
+
+       
+
+        //...
+    }
+
+    private void DoMovement()
+    {
+        Vector3 displace = _velocity * Time.deltaTime;
+
+        _charCtrl.Move(displace);
+    }
 }
